838adaea6a6df25c9b801c02218ad9b8c2959a27
braemsje <jerome.braems@utt.fr>
2022-11-23 11:45:40 +0100

    added interfaces methods

--- src/brutal/IGame.java
+++ src/brutal/IGame.java
@@ -1,5 +1,39 @@
 package brutal;
 
-public interface IGame {
+import java.util.List;
 
+public interface IGame {
+	public void initializeGame();
+	
+	public void setState(IState state);
+	
+	public void createPlayer(Program program);
+	
+	public void createStudent(Player player, int strength, int dexterity, int resilience, int constitution, int initiative);
+	
+	public void setStudentAsReservist(IStudent student);
+	
+	public void setEliteStudent(IStudent student);
+	
+	public void setMasterStudent(IStudent student);
+	
+	public void placeStudentOnArea(IStudent student, Area area);
+	
+	public void placeReservist(IStudent reservist, Area area);
+	
+	public List<Player> getPlayers();
+	
+	public Player getPlayerTurn();
+	
+	public void switchPlayerTurn();
+	
+	public boolean isStudentBelongingToPlayer(IStudent student, Player player);
+	
+	public List<Area> getAreas();
+	
+	public Area getAreaByName(String name);
+	
+	public Player getAreaOwner(Area area);
+	
+	public Player getWinner();
 }
--- src/brutal/IState.java
+++ src/brutal/IState.java
@@ -1,5 +1,9 @@
 package brutal;
 
 public interface IState {
-
+	public void updateGameState(IGame game);
+	
+	public void executeCommandLine(String command, IGame game);
+	
+	public boolean commandIsValid(String command);
 }
