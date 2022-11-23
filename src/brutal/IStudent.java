23430fc395a279ae0e776fe47c7ed09d6bd3681e
AntoineLOO2 <ptsi.antoine@gmail.com>
2022-11-23 11:42:56 +0100

    methodes Istudient

--- src/brutal/IStudent.java
+++ src/brutal/IStudent.java
@@ -1,5 +1,33 @@
 package brutal;
 
 public interface IStudent {
-	public void setStrategy(Area area, IGame game);
+	public void useStrategy(Area area, IGame game);
+	
+	public void setStrategy(IStrategy strategy);
+	
+	public void setStrength(int strength);
+	
+	public void setDexterity(int dexterity);
+	
+	public void setResilience(int resilience);
+	
+	public void setConstitution(int constitution);
+	
+	public void setInitiative(int initiative);
+	
+	public void heal(int ects);
+	
+	public void damage(int ects);
+	
+	public int getEcts();
+	
+	public int getStrength();
+	
+	public int getDexterity();
+	
+	public int getResilience();
+	
+	public int getConstitution();
+	
+	public int getInitiative();
 }
